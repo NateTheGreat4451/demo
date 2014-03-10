b33a85cbbbd7d9acf6eb679a39d413530d6664aa
GitHub Sample Teacher <training+githubtrainer@github.com>
2014-03-10 17:17:41 -0600

    add some stuff

--- hello.rb
+++ hello.rb
@@ -1,7 +1,8 @@
-words = %w(awesome amazing neat)
+words = %w(awesome amazing cool neat)
 word = words[rand(words.length)]
 
 print "what is your name?"
 name = gets
 
 puts "hello, #{name}! You are #{word}!"
+puts "teacher says hello"
